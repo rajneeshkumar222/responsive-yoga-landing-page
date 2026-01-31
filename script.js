e3a23f1944b4994d68dbfeec57ff536fc367be94
Rajneesh Kumar Maurya <mauryarajneesh@gmail.com>
2026-01-31 12:42:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+ScrollReveal().reveal('.hero-content', {
+  distance: '60px',
+  origin: 'left',
+  duration: 1000,
+  easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal('.hero-img', {
+  distance: '60px',
+  origin: 'right',
+  duration: 1000,
+  delay: 300,
+  easing: 'ease-in-out'
+});
